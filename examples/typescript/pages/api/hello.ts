4d6cc37402481b8e78875c2bfb55b867c7cb4b85
alan910127 <alan469357@gmail.com>
2023-01-22 15:02:12 +0800

    :memo: Add example next.js project

---
+++ examples/typescript/pages/api/hello.ts
@@ -0,0 +1,34 @@
+import { createEndpoint, procedure } from "@alan910127/next-api-builder";
+import { randomUUID } from "crypto";
+import { z } from "zod";
+
+export default createEndpoint({
+  get: procedure
+    .query(
+      z.object({
+        text: z.string().optional(),
+      })
+    )
+    .handler(async (req, res) => {
+      const text = req.query.text ?? "world";
+      res.status(200).send(`Hello ${text}`);
+    }),
+
+  post: procedure
+    .body(
+      z.object({
+        name: z.string(),
+        age: z.number().nonnegative(),
+      })
+    )
+    .handler(async (req, res) => {
+      const { name, age } = req.body;
+      res.status(201).json({
+        id: randomUUID(),
+        name,
+        age,
+      });
+    }),
+
+  // ...put, delete etc.
+});
